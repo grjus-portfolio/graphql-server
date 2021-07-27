a5a8d84358253b7e2970c088c2e34d424896615a
grjus@pm.me <grjus@pm.me>
2021-07-27 21:22:13 +0000

    Added rocket resolver

--- server/entities/launch.entity.ts
+++ server/entities/launch.entity.ts
@@ -1,4 +1,21 @@
+/* eslint-disable camelcase */
 import { Field, ObjectType } from 'type-graphql'
+import spacexApi from '../api'
+
+@ObjectType()
+class Rocket {
+    @Field(of => String!)
+    name:string
+
+    @Field(of => String)
+    country:string
+
+    @Field(of => String!)
+    cost_per_launch:string
+
+    @Field(of => String!)
+    description:string
+}
 
 @ObjectType()
 class Launch {
@@ -13,6 +30,12 @@ class Launch {
 
     @Field(of => String!)
     rocket:string
+
+    @Field(of => Rocket)
+    async rocket_data () {
+      const response = await spacexApi(`/rockets/${this.rocket}`)
+      return response.data
+    }
 }
 
 export { Launch }
--- server/resolvers/launch.resolver.ts
+++ server/resolvers/launch.resolver.ts
@@ -1,12 +1,19 @@
-import { Query, Resolver } from 'type-graphql'
+import { Arg, Query, Resolver } from 'type-graphql'
 import spacexApi from '../api'
 import { Launch } from '../entities/launch.entity'
 
 @Resolver()
 class LaunchResolver {
   @Query(() => [Launch])
-  async launches ():Promise<Launch> {
+  async launches (@Arg('name', type => String, { nullable: true }) name:string = ''):Promise<Launch> {
     const response = await spacexApi('/launches')
+    const output = response.data.filter((item:Launch) => item.name.toLowerCase().includes(name.toLowerCase()))
+    return output
+  }
+
+  @Query(() => Launch)
+  async launch (@Arg('launch_id', type => String) launchId:string) {
+    const response = await spacexApi(`/launches/${launchId}`)
     return response.data
   }
 }
